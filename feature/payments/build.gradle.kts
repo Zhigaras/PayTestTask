9865ed846e836f72ac5c4fe8aec8bb415a65846d
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-30 18:49:48 +0600

    payments module created

---
+++ feature/payments/build.gradle.kts
@@ -0,0 +1,44 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.zhigaras.payments"
+    compileSdk = Config.compileSdk
+    
+    defaultConfig {
+        minSdk = Config.minSdk
+        
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+    
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
